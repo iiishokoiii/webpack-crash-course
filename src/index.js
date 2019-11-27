c2544722f86e966d42857d8ce7db57c3c2a83b19
織田翔子 <odashoko@odashoukonoMacBook-puro.local>
2019-11-28 00:00:42 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import _ from 'lodash'
+// import { NAME, sum } from './utilities'
+import * as utilities from './utilities'
+import { NAME as utilitiesName } from './utilities'
+
+console.log(utilities.sum(1, 2))
+console.log(utilities.NAME)
+console.log(utilitiesName)
 function component() {
   const elm = document.createElement('div')
   const array = ['Hello', 'webpack', '!!']
@@ -6,4 +13,4 @@ function component() {
   return elm
 }
 
-document.body.appendChild(component())  
+document.body.appendChild(component())
